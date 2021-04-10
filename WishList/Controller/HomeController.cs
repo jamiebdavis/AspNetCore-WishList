4988316c610504e80e45ca971965fa6fbc5cd34d
Jamie Davis <jamie.davis@ao.com>
2021-04-10 15:17:36 +0100

    changed controller

--- WishList/Controller/HomeController.cs
+++ WishList/Controller/HomeController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WishList.Controller
 {
-    public class HomeController : Controller
+    public class HomeController : Microsoft.AspNetCore.Mvc.Controller
     {
         public IActionResult Index()
         {
